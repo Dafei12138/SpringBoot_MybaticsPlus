4ce32e9c6a289eda6d6c135488e19b296153505f
leo_hf <1810316975@qq.com>
2018-10-09 20:21:22 +0800

    规范化

--- sbmp_api/src/main/java/com/sbmp/api/controller/SwaggerDemoController.java
+++ sbmp_api/src/main/java/com/sbmp/api/controller/SwaggerDemoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by Administrator on 2017/9/13.
  */
+
 @RestController
 @RequestMapping("/api")
 @Api("swaggerDemoController相关的api")
